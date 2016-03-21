d7095d8bccf8e417be0261d6f519f5334bf535be
sylar jiang <5246396@qq.com>
2016-03-21 23:53:09 +0800

    modified:   day1/job.py

--- day1/job.py
+++ day1/job.py
@@ -5,50 +5,55 @@ pwd_menu = { #account list ant count
     'jerry' : ['12345678',0],
     'sylar' : ['123456789',0]
 }
-print pwd_menu
+
 logon_flag = False
 lock_user = []
 file_name = r'E:/\python-file/\day1/\lock_list.txt'#choose file
-if os.path.exists(file_name) == True:
+
+if os.path.exists(file_name) :#read lock file
     lock_read = file(file_name,'r').read()
-    lock_user = lock_read.split()
-    print lock_user
+    if lock_read.strip():
+        lock_user = lock_read.split()
+
 for name_count in range(3):
+
     user_name =raw_input("Please input you username:").strip()
     if len(user_name) == 0:
         continue
     if user_name in lock_user:#username locked
-        print "You is locked"
-        logon_flag = True
+        print "You are locked"
         break
     if pwd_menu.has_key(user_name):
-        logon_count = int(pwd_menu[user_name][1])
-        print logon_count
+        logon_count = int(pwd_menu[user_name][1])#count read
+
         for pwd_count in range(3):
-            user_pwd = str(raw_input("Please input you passwd:").strip())
-            print type(user_pwd)
+            user_pwd = str(raw_input("Please input you passwd:").strip())#pwd
             logon_count += 1
-            print logon_count
             if len(user_pwd) == 0:
                 continue
             if user_pwd == str(pwd_menu[user_name][0]):
                 #while True:
-                print "Welcome logon"
-
-                '''else:
+                user_cmd=raw_input( "Welcome logon! Doing something!,input 'q' exit").strip()
+                if user_cmd == 'q':
                     logon_flag = True
-                    break'''
+                break
+
     if logon_flag:
-        print "Logout,see you next time!"
+        print "Logout,See you next time!"
         break
     if int(logon_count) == 3:
         print "Lock this user!"
-        lock_user.append(user_name)
-        lock_file = open(file_name,'w')
-        lock_file.write(repr(lock_user))
-        lock_file.close()
-        print lock_user
-        break
+        if user_name in lock_user:
+            break
+        else:
+            lock_user.append(user_name)
+            print lock_user
+            lock_file = open(file_name,'w')
+            lock_file.write(user_name)
+            lock_file.close()
+            break
+
+
 
 
 
