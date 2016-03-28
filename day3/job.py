90bae3d005afe1c060c38f574f060d1cc02383b7
sylar jiang <5246396@qq.com>
2016-03-28 16:49:34 +0800

    new file:   day3/job.py

---
+++ day3/job.py
@@ -0,0 +1,19 @@
+#_*_coding:utf-8_*_
+
+def web_login():  #web登录选购商品
+    pwd_menu = {
+        'name' : ['alex','tom','jerry','sylar'],
+        'psswd' : ['123456','1234567','12345678','123456789']
+    }
+
+
+
+
+
+
+
+
+
+
+
+
